b5564855048e07f689a1b978ce8621f33f5b9002
Bassam AbouZaid <BassamAZ@Bassams-MacBook-Pro.local>
2019-01-03 23:15:27 +0400

    Modify models

--- fms-vehicle/src/main/java/com/alten/fms/model/Vehicle.java
+++ fms-vehicle/src/main/java/com/alten/fms/model/Vehicle.java
@@ -2,15 +2,14 @@ package com.alten.fms.model;
 
 import java.time.LocalDateTime;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehicle {
 	
 	@Id
@@ -22,50 +21,60 @@ public class Vehicle {
 	private String registrationNumber;
 	private String status;
 	private LocalDateTime latUpdatedDate;
+
 	public String getId() {
 		return id;
 	}
-	public void setId(String id) {
-		this.id = id;
-	}
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public String getModel() {
 		return model;
 	}
-	public void setModel(String model) {
-		this.model = model;
-	}
+
 	public String getPlateNumber() {
 		return plateNumber;
 	}
-	public void setPlateNumber(String plateNumber) {
-		this.plateNumber = plateNumber;
-	}
+
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
-	public void setRegistrationNumber(String registrationNumber) {
-		this.registrationNumber = registrationNumber;
-	}
+
 	public String getStatus() {
 		return status;
 	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+
 	public LocalDateTime getLatUpdatedDate() {
 		return latUpdatedDate;
 	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public void setLatUpdatedDate(LocalDateTime latUpdatedDate) {
 		this.latUpdatedDate = latUpdatedDate;
 	}
-	
-	
-
-
 }
