81a4b5a40587f975a8c5a9e3f40884664fb9d2bd
Bassam AbouZaid <>
2019-01-07 18:23:38 +0400

    update vehicle service testing

--- fms-vehicle/src/test/java/com/alten/fms/service/VehicleServiceTest.java
+++ fms-vehicle/src/test/java/com/alten/fms/service/VehicleServiceTest.java
@@ -2,55 +2,94 @@ package com.alten.fms.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.alten.fms.model.Vehicle;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class VehicleServiceTest {
 
+	@Autowired
 	VehicleService vehicleService;
 
+	@org.junit.Test
+	public void testFindAll() {
 
-	@Test
-	void testFindAll() {
-		vehicleService= new VehicleService();
-		vehicleService.findDummyVehicle();
-		Assert.assertEquals("done","done");
+		List<Vehicle> customerList=vehicleService.findAll();
+		Assert.assertEquals(customerList.size(),customerList.size());
 
 	}
 
-	@Test
-	void testFindById() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test(expected = IllegalArgumentException.class)
+	public void testFindByIdWithEmptyValue() {
+		vehicleService.findById("");
 	}
 
-	@Test
-	void testFindByRegistrationNumber() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test
+	public void testFindByIdWithExitingValue() {
+		Vehicle vehicle=vehicleService.findById("5c2e7c423650c5ca72a8e661");
+
+		Assert.assertEquals(vehicle.getModel(),"bmw");
+
 	}
 
-	@Test
-	void testFindByPlateNumber() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test(expected = IllegalArgumentException.class)
+	public void testFindByNameWithEmptyValue() {
+		vehicleService.findByName("");
 	}
 
-	@Test
-	void testFindByName() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test
+	public void testFindByNameWithExitingValue() {
+		List<Vehicle> vehicle=vehicleService.findByName("cartest");
+
+		Assert.assertEquals(vehicle.size(),vehicle.size());
+
 	}
 
-	@Test
-	void testPulse() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test(expected = IllegalArgumentException.class)
+	public void testFindByPlateNumberWithEmptyValue() {
+		vehicleService.findByPlateNumber("");
 	}
 
-	@Test
-	void testFindDummyVehicle() {
-		Assert.assertEquals("done","done");
+	@org.junit.Test
+	public void testFindByPlateNumberWithExistingValue() {
+		Vehicle vehicle=vehicleService.findByPlateNumber("1234");
+
+		Assert.assertEquals(vehicle.getModel(),"bmw");
+
 	}
+
+	@org.junit.Test(expected = IllegalArgumentException.class)
+	public void testFindByRegistrationNumberWithEmptyValue() {
+		vehicleService.findByRegistrationNumber("");
+	}
+
+	@org.junit.Test
+	public void testFindByRegistrationNumberWithExistingValue() {
+		Vehicle vehicle=vehicleService.findByRegistrationNumber("4321");
+
+		Assert.assertEquals(vehicle.getModel(),"bmw");
+
+	}
+
+
+	@org.junit.Test
+	public void testFindDummyCustomer() {
+		Vehicle vehicle=vehicleService.findDummyVehicle();
+
+		Assert.assertEquals(vehicleService,vehicleService);
+	}
+
+
+
 
 }
